59c43ae59f1aaae8130e671c685e8af47201cbab
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-07-27 14:39:31 +0100

    analysis and validation completed

--- task-two/src/validation.ts
+++ task-two/src/validation.ts
@@ -8,7 +8,7 @@ import fs from 'fs';
 import dns from 'dns';
 let pathPromise: Promise<string>;
 async function validateEmailAddresses(inputPath: string[], outputFile: string) {
-  let path = inputPath[0];
+  const path = inputPath[0];
   pathPromise = new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
@@ -18,15 +18,15 @@ async function validateEmailAddresses(inputPath: string[], outputFile: string) {
       }
     });
   });
-  let fileContent: string = await pathPromise;
-  let lines = fileContent.split('\n');
-  let header = lines.splice(0, 1);
-  let footer = lines.splice(-1);
-  let validatedEmailPromises: unknown[] = [];
-  for (let email of lines) {
+  const fileContent: string = await pathPromise;
+  const lines = fileContent.split('\n');
+  const header = lines.splice(0, 1);
+  const footer = lines.splice(-1);
+  const validatedEmailPromises: unknown[] = [];
+  for (const email of lines) {
     const domain = email.split('@')[1];
 
-    let val: Promise<unknown> = new Promise((resolve, reject) => {
+    const val: Promise<unknown> = new Promise((resolve, reject) => {
       dns.resolve(domain, 'MX', function (err) {
         if (err) {
           resolve(null);
@@ -38,11 +38,11 @@ async function validateEmailAddresses(inputPath: string[], outputFile: string) {
     validatedEmailPromises.push(val);
   }
 
-  let validatedEmail: string[] = (await Promise.all(
+  const validatedEmail: string[] = (await Promise.all(
     validatedEmailPromises,
   )) as string[];
-  let validEmailDomains: string[] = validatedEmail.filter((x) => x !== null);
-  let outPut = [...header, ...validEmailDomains, ...footer].join('\n');
+  const validEmailDomains: string[] = validatedEmail.filter((x) => x !== null);
+  const outPut = [...header, ...validEmailDomains, ...footer].join('\n');
   return fs.writeFile(outputFile, outPut, 'utf8', (err) => {
     if (err) {
       console.log(err);
