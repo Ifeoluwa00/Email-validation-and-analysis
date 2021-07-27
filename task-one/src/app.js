d1d64bc9ec3913ab48e3ddb8f969fc76dd807c01
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-07-27 14:32:07 +0100

    analysis and validation co

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -2,8 +2,25 @@ import FileTree from './fileTree';
 
 export function createFileTree(input) {
   const fileTree = new FileTree();
+  let arrWIthParentId = []
+  let arrWithNoParentId = []
+  for (let value of input) {
+    if (value.parentId === undefined) {
+      arrWithNoParentId.push(value)
+    }
+    else {
+      arrWIthParentId.push(value)
+    }
 
-  for (const inputNode of input) {
+  }
+  arrWIthParentId = arrWIthParentId.sort((a, b) => a.id - b.id)
+  let jointUser = [...arrWithNoParentId, ...arrWIthParentId ] //arrWithNoParentId.concat(arrWIthParentId)
+  
+  //let jointUser = input.sort((a,b)=> a.parentId - b.id)
+  
+
+
+  for (const inputNode of jointUser) {
     const parentNode = inputNode.parentId
       ? fileTree.findNodeById(inputNode.parentId)
       : null;
--- task-two/src/validation.ts
+++ task-two/src/validation.ts
@@ -4,8 +4,57 @@
  * @param {string[]} inputPath An array of csv files to read
  * @param {string} outputFile The path where to output the report
  */
-function validateEmailAddresses(inputPath: string[], outputFile: string) {
-  console.log('Complete the implementation in src/validation.ts');
+import fs from "fs";
+import dns from 'dns';
+let pathPromise: Promise<string>;
+async function validateEmailAddresses(inputPath: string[], outputFile: string) {
+  let path = inputPath[0]
+  pathPromise = new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+  let fileContent: string = await pathPromise
+  let lines = fileContent.split("\n")
+  let header = lines.splice(0, 1)
+  let footer = lines.splice(-1)
+  let validatedEmailPromises: unknown[]= []
+  for (let email of lines) {
+    
+    const domain = email.split("@")[1]
+    
+    let val: Promise<unknown> = new Promise((resolve, reject) => {
+      dns.resolve(domain, 'MX', function (err) {
+        if (err) {
+          resolve(null)
+        } else {
+          resolve(email)
+        }
+      })
+    })
+    validatedEmailPromises.push(val)
+  }
+
+  let validatedEmail: string[] = await Promise.all(validatedEmailPromises) as string[]
+  let validEmailDomains: string[] = validatedEmail.filter(x => x !== null) 
+  let outPut = [...header,...validEmailDomains,...footer].join('\n')
+  return fs.writeFile(outputFile, outPut, 'utf8',(err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+  
 }
 
+  
+// console.log(validateEmailAddresses(["./fixtures/inputs/small-sample.csv"], ""));
 export default validateEmailAddresses;
+
+
+
+
+   
